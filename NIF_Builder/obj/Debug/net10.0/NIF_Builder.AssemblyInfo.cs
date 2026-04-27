1ffbb0ef77c7b3a5c7c391c2092d225f87e8ffa7
Nakibul Islam Fahim <110785762+nifahim4@users.noreply.github.com>
2026-04-28 00:27:15 +0600

    1227280426

--- NIF_Builder/obj/Debug/net10.0/NIF_Builder.AssemblyInfo.cs
+++ NIF_Builder/obj/Debug/net10.0/NIF_Builder.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NIF_Builder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a65a54d56c00902b5c1044ccaaaffb30e16192e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5620efac4f0568b7d51feb62588fc4c543df6a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("NIF_Builder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NIF_Builder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
