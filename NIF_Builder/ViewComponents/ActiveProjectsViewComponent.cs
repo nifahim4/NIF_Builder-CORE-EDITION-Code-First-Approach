7a65a54d56c00902b5c1044ccaaaffb30e16192e
Nakibul Islam Fahim <110785762+nifahim4@users.noreply.github.com>
2026-04-28 00:17:11 +0600

    1216280426

---
+++ NIF_Builder/ViewComponents/ActiveProjectsViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NIF_Builder.Data;
+
+namespace NIF_Builder.ViewComponents
+{
+    public class ActiveProjectsViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ActiveProjectsViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var activeCount = await _context.Projects.CountAsync(p => p.WorkInProgress);
+
+            return View(activeCount);
+        }
+    }
+}
